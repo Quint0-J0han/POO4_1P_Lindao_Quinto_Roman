9adb02cb74fa19681af188ebef9128b7126607f0
AxelRoman1 <150213728+AxelRoman1@users.noreply.github.com>
2024-06-20 14:06:30 -0500

    metodo iniciar sesion y agregar getters y setters

--- src/Main/clasesP/Usuario.java
+++ src/Main/clasesP/Usuario.java
@@ -7,14 +7,66 @@ public class Usuario {
     private String correoElectronico;
     private String usuario;
     private String contraseña;
-    private char rol;
+    private String rol;
 
-    public Usuario(String nombre, String apellido, String correoElectronico, String usuario, String contraseña, char rol) {
+    public Usuario(String nombre, String apellido, String correoElectronico, String usuario, String contraseña, String rol) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.correoElectronico = correoElectronico;
         this.usuario = usuario;
         this.contraseña = contraseña;
+        this.rol = rol;
+    }
+  public void iniciarSesion() {
+        System.out.println("Iniciando sesión como " + this.usuario);
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public char getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
         this.rol = rol;
     }
 }
