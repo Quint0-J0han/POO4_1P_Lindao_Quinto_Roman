ddd6b7062aa58ced062daeb666d53ad4b4fc2de7
AxelRoman1 <150213728+AxelRoman1@users.noreply.github.com>
2024-06-20 14:04:34 -0500

    agregar atributos y constructor usuario

--- src/Main/clasesP/Usuario.java
+++ src/Main/clasesP/Usuario.java
@@ -2,4 +2,19 @@ package clasesP;
 import java.util.Scanner;
 
 public class Usuario {
+  private String nombre;
+    private String apellido;
+    private String correoElectronico;
+    private String usuario;
+    private String contraseña;
+    private char rol;
+
+    public Usuario(String nombre, String apellido, String correoElectronico, String usuario, String contraseña, char rol) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correoElectronico = correoElectronico;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
 }
