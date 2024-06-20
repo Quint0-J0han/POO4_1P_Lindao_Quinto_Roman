88a7aab67221d08776bf4cc49927c2ee702393d4
fernandolindao-m <flindao@espol.edu.ec>
2024-06-20 13:41:57 -0500

    Update Editor.java

--- src/Main/clasesP/Editor.java
+++ src/Main/clasesP/Editor.java
@@ -1,1 +1,10 @@
+package clasesP
+public class Editor extends Usuario {
+    private List<Articulo> articulosAsignados;
 
+    public Editor(String nombre, String correoElectronico, String contraseña, int numeroIdentificacion) {
+        super(nombre, correoElectronico, contraseña, numeroIdentificacion);
+        this.articulosAsignados = new ArrayList<>(); // Lista para almacenar los artículos asignados
+    }
+
+    // Métodos específicos para la clase Editor (asignarRevisor(), enviarComentario(), etc.)
