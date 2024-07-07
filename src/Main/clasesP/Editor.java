a7d8975b2816fb80523d1c338398c42fdb1bd06f
AxelRoman1 <asroman@espol.edu.ec>
2024-07-07 18:29:34 -0500

    muestra commit

--- src/Main/clasesP/Editor.java
+++ src/Main/clasesP/Editor.java
@@ -6,7 +6,15 @@ public class Editor extends Usuario {
     public Editor(String nombre, String correoElectronico, String contraseña, int numeroIdentificacion) {
         super(nombre, correoElectronico, contraseña, numeroIdentificacion);
         this.articulosAsignados = new ArrayList<>(); // Lista para almacenar los artículos asignados
+    //hola nuevo cambio
+    System.out.println("hola");
+
+//hola x2 
+
     }
+//System.out.println("hola");
+
+
 }
 */
     // Métodos específicos para la clase Editor (asignarRevisor(), enviarComentario(), etc.)
