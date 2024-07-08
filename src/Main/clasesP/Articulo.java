a8cafaafcd283cc461c9d73d0bfee129e82cc65d
Quint0J0han <jaquinto@espol.edu.ec>
2024-07-07 19:35:01 -0500

    Se agrego la clase Revisor y se mejoro Articulos

--- src/Main/clasesP/Articulo.java
+++ src/Main/clasesP/Articulo.java
@@ -1,45 +1,28 @@
 package clasesP;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.security.SecureRandom;
 
 public class Articulo {
-    /*
-     * 
-     * un articulo solo tiene un autor
-     * tienen un codigo unico que genera el sistema
-     * datos ingresados (titulo, resumen, contenido, palabras clave)
-     * Luego, enviarse a revision
-     * 
-     */
-    private int codigoUnico=(int)(Math.random()*1000); //genera un codigo para el articulo
-    private String titulo,resumen,contenido,wordClave;
-    /*
-     * 
-     * Usuario persona = new Usuario(); llama a la clase usuario
-     * Autor escritor = new Autor();
-     * 
-     * nombre = persona.getNombre(); Se toma el nombre de la clase Usuario
-     * codigoID = escritor.getCodigoID(); Se toma su codigo de ID de la clase Autor
-     * 
-     */
-    Articulo(String nombre,int codigoID,int codigoUnico){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el titulo de su obra: ");
-        titulo = sc.next();
-        sc.next();
-        System.out.println("Ingrese el resumen: ");
-        resumen = sc.nextLine();
-        sc.next();
-        System.out.println("Ingrese el contenido: ");
-        contenido = sc.nextLine();
-        sc.next();
-        System.out.println("Ingrese las palabras clave: ");
-        wordClave = sc.nextLine();
-        sc.close();
 
+    private String titulo,codigo,resumen,contenido; //como se conforme un articulo
+    private Autor autor;  //sera valido cuando se agregue la clase AUTOR
+    private String[] palabrasClave;
+    
+    //Se inicializan las variables
+    public Articulo(String titulo,String resumen,String contenido,Autor autor){
+        this.titulo = titulo;
+        this.resumen = resumen;
+        this.contenido = contenido;
+        this.autor = autor;
+        this.codigo = generarCodigo();
     }
 
+    //metodos get and set
     public String getTitulo(){
         return titulo;
+    }
+    public String getCodigo(){
+        return codigo;
     }
     public String getResumen(){
         return resumen;
@@ -47,8 +30,47 @@ public class Articulo {
     public String getContenido(){
         return contenido;
     }
-    public String getWordClave(){
-        return wordClave;
+    public Autor getAutor(){
+        return autor;
+    }
+    public String[] getPalabrasClave(){
+        return palabrasClave;
+    }
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+    public void setResumen(String resumen){
+        this.resumen = resumen;
+    }
+    public void setContenido(String contenido){
+        this.contenido = contenido;
+    }
+    public void setAutor(Autor autor){
+        this.autor = autor;
+    }
+    public void setPalabrasClave(String[] palabrasClave){
+        this.palabrasClave = palabrasClave;
+    }
+
+    //Genracion de codigo aleatorio 
+    private String generarCodigo(){
+        String Caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom Random = new SecureRandom();
+        StringBuilder codigo = new StringBuilder(6);
+        for(int i;i<6;i++){
+            int indiceAleatorio = Random.nextInt(Caracteres.length());
+            codigo.append(Caracteres.charAt(indiceAleatorio));
+        }
+        return codigo.toString();
+    }
+    @Override
+    public String toString(){
+        return "Articulo [titulo: "+titulo+", codigo: "+codigo+
+        ", resumen: "+resumen+", contenido: "+", autor: "+autor+", palabras clave: "+
+        Arrays.toString(palabrasClave)+"]";
     }
 
 }
---
+++ src/Main/clasesP/Revisor.java
@@ -0,0 +1,32 @@
+package clasesP;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class Revisor extends Usuario {
+    String especialidad;
+    int numeroArticulos;
+    public Revisor(String user,String Contrasenia,char rol,String nombre,
+    String apellido,String especialidad,int numeroArticulos){
+        super(user,Contrasenia,rol,nombre,apellido);
+        GenerarCorreo();
+    }
+    
+    
+    
+    @Override
+    public void GenerarCorreo(){
+        this.setCorreoElectronico(this.getUser()+"@gmail.com");
+    }
+
+    public void SubirRevision(Articulo arti, String comentarios){
+        try(BufferedWriter write = new BufferedWriter(
+            new FileWriter("src/Main/archivos/Revision.txt",true))){
+                String contenido = this.getNombre()+","+arti.getCodigo()+", comentarios: "
+                +comentarios;
+                writer.write(contenido);
+                System.out.println("Articulo puesto en revision");
+            }catch(Exception e){ 
+                System.out.println("Articulo no subido");
+            }
+    }
+}
